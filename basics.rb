04d6bfc214d8144f54995e2ea06c73359214cf9e
William Tran <william.th.tran@gmail.com>
2015-04-08 04:32:39 +0000

    Uploading exercises

---
+++ basics.rb
@@ -0,0 +1,39 @@
+#1 Funny how it stands for a comment and a way for me to number these problems
+puts 'William' + ' ' + 'Tran'
+
+#2
+9876/1000 #thousands number
+9876 % 1000 /100 #hundreds number
+9876 % 1000 % 100 % /10 #tens number
+9876 % 1000 % 100 % 10 #ones number
+
+#3
+movies = {'Still in the Womb' => 1975, '4 more years of Bush' => 2004, 'Living in Connecticut' 2013, 'Terrible Year in America' => 2001, 'Give me five more years' => 1981}
+puts movies[:'Still in the Womb']
+puts movies[:'4 more years of Bush']
+puts movies[:'Living in Connnecticut']
+puts movies[:'Terrible Year in America']
+puts movies[:'Give me five more years']
+  
+#4
+movies = ['Still in the Womb', 1975, '4 more years of Bush', 2004, 'Living in Connecticut', 2013, 'Terrible Year in America', 2001, 'Give me five more years',1981]
+  puts movies[1]
+  puts movies[3]
+  puts movies[5]
+  puts movies[7]
+  puts movies[9]
+  #Ha. Made my answer more complicated than what it was asking. Oh well.
+
+#5
+puts 5*4*3*2*1
+puts 6*5*4*3*2*1
+puts 7*6*5*4*3*2*1
+puts 8*7*6*5*4*3*2*1
+  
+#6
+puts 12.9 **2
+puts 13.1 **2
+puts 7843827**2
+
+#7
+There needs to be a closed bracket since there is an open bracket in the program.
